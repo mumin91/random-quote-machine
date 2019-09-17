ce820c3584c14003604fd1079935ddfe8cec60d7
Muminur Rahman <r.muminur@gmail.com>
2019-09-18 01:43:40 +0600

    Updates

---
+++ precache-manifest.982909d16c4a10dacb108f07693b77da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a772225d2ccb37e1e2dd51eaa3cdeae",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "e2877cc996bd20b44ec9",
+    "url": "/random-quote-machine/static/css/main.1609a22c.chunk.css"
+  },
+  {
+    "revision": "3c1eaadf93662a52eacb",
+    "url": "/random-quote-machine/static/js/2.bfa4f964.chunk.js"
+  },
+  {
+    "revision": "e2877cc996bd20b44ec9",
+    "url": "/random-quote-machine/static/js/main.9943a6e1.chunk.js"
+  },
+  {
+    "revision": "2fe14a2688eb1cce5628",
+    "url": "/random-quote-machine/static/js/runtime~main.b546f5f2.js"
+  }
+]);
